20deada39618dcdc341216910178f5b6991b1721
Paulus de Vries <paulusdevries@gmail.com>
2019-07-30 12:37:03 +0200

    Api Read adapted

--- jira_api.py
+++ jira_api.py
@@ -1,19 +1,25 @@
 # This code sample uses the 'requests' library: # http://docs.python-requests.org
 import requests, getpass
+import urllib3
 from requests.auth import HTTPBasicAuth
 import json
+from save_game import SaveGame
 
-sprintId = 23645
+sprintId = int(input("Geef het ID van de sprint op: "))
 propertyKey = "goal"
+urllib3.disable_warnings()
 url = f"https://jira.kpn.org/rest/agile/1.0/sprint/{sprintId}/"
-auth = HTTPBasicAuth("paulusdevries@kpn.com",password=getpass.getpass())
-
+auth = HTTPBasicAuth("paulusdevries@kpn.com", password=getpass.getpass())
 response = requests.request(
     "GET",
     url,
     auth=auth,
     verify=False
 )
+
+saveText = SaveGame()
+
+saveText.saveGame(response.text)
 
 text = json.loads(response.text)
 
